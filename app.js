b88c004237e15e3735d57dfb8c2264eb1b3defe4
chandra <santprakash28877@gmail.com>
2025-07-12 13:51:07 +0530

    auth endpoints added

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const morgan = require('morgan');
+const cors = require('cors');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const authRoutes = require('./src/routes/auth.routes');
+require('dotenv').config();
 
+const connectDB = require('./config/database');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: 'Something went wrong!'
+    });
+});
+
+
+app.use('/v1/auth', authRoutes);
 app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
     console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
